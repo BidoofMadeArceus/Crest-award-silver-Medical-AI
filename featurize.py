c9d7d4ba3699cb302f4c31c988e3ad2a00e62e26
Adam Karim <adamkhalilkarim@gmail.com>
2026-02-01 14:54:25 +0000

    Create featurize.py

---
+++ featurize.py
@@ -0,0 +1,10 @@
+import numpy as np
+from rdkit.chem import allchem
+
+def make_morgan_fp(mol, radius=2, nBits=1024):
+  fp= AllChem.GetMorganFingerprintAsBitVect(
+    mol,
+    radius=radius,
+    nBits=nBits
+  )
+  return np.array(fp)
