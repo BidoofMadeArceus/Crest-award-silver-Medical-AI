4fffa60bffbc345a208df9257173ea0607356f62
Adam Karim <adamkhalilkarim@gmail.com>
2026-02-01 12:10:16 +0000

    Update Extract.py

--- Extract.py
+++ Extract.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
 df = pd.read_csv("dataset.csv")
+df = df.dropna(subset=["SMILES"])
+
+smiles_series = df["SMILES"]
+smiles_list = df["SMILES"].tolist()
+
+from rdkit import Chem
+
+df["Mol"] = df["SMILES"].apply(Chem.MolFromSmiles)
 
 print(df)
