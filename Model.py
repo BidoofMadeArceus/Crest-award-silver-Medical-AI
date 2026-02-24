5ab77c14092d1f09e06ccc004929302f51ade94b
Adam Karim <158174701+UnsurpassedRobot@users.noreply.github.com>
2026-02-24 13:23:29 +0000

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,6 +1,10 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import RandomForestRegressor
 from TestSplit import X_train
 from TestSplit import y_train
 
-model = LinearRegression()
+model = RandomForestRegressor(
+    n_estimators=200,
+    random_state=42
+)
+
 model.fit(X_train, y_train)
