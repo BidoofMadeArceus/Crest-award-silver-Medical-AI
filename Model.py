2514056ce9400c2bb88489ad43580f870db8f46f
Adam Karim <158174701+UnsurpassedRobot@users.noreply.github.com>
2026-02-24 13:18:38 +0000

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,4 +1,6 @@
 from sklearn.linear_model import LinearRegression
+from TestSplit import X_train
+from TestSplit import y_train
 
 model = LinearRegression()
-model.fit(X_train, y_train
+model.fit(X_train, y_train)
