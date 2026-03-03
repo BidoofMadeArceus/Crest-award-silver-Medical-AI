eb81b5d43fef184bb99cfdbbc14fa03f4d786081
Adam Karim <158174701+UnsurpassedRobot@users.noreply.github.com>
2026-03-03 13:40:24 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ from TrainSplit import y_test
 from sklearn.metrics import mean_squared_error
 
 predictions = model.predict(X_test)
-print(predictions)
+print(10 ** predictions)
 
 error = mean_squared_error(y_test, predictions)
 print("Error:", error)
