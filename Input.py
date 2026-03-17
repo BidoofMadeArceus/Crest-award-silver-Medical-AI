16743341933250e86fd4d8563c8e7285847ed7cf
Adam Karim <158174701+UnsurpassedRobot@users.noreply.github.com>
2026-03-17 13:46:06 +0000

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,2 +1,10 @@
 from Model import model
 from featurize import make_morgan_fp
+
+predictThis = input("please input valid SMILES code for LD50 prediction: ")
+
+make_morgan_fp(predictThis)
+
+prediction = model.predict(fp)
+
+print(prediction)
